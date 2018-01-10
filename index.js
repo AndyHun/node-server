82679e9d09b7a1f612d795a1cf1213f6fae7b096
yih <Yi.Huang@ehealth-china.com>
2018-01-10 17:40:27 +0800

    use specific format

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+   
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
